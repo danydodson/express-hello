61c2762546b52ada742eef8c18f6186c5c695fa1
Dany Dodson <danydodson@gmail.com>
2020-03-15 22:46:36 -0500

    modified res.send

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const express = require("express")
 const app = express()
 const port = 80
 
-app.get("/", (req, res) => res.send("Hello there from express ! update 02\n"))
+app.get("/", (req, res) => res.send("Hello from express: update 02\n"))
 app.listen(port, () => console.log(`App started on http://localhost:${port}`))
